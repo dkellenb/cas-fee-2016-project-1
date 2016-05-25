4e1ca8d316eca799e88774cadc80b28909b7fb41
Daniel Kellenberger <daniel.kellenberger@netzgestaltung.ch>
2016-05-25 10:46:45 +0200

    - add jquery, momentjs and handlebar - use handlebar template - define model

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/**
+ * Notes application
+ */
+
+(function ($) {
+    $(function () {
+        var notes = [
+            {
+                id: 1,
+                title: "CAS-FEE Projekt bearbeiten",
+                content: "Hier ist ein Text welcher einen möglichen Inhalt einer Notiz darstellt. Dieser kann relative sehr gross sein. Die Idee ist dass durch einen Klick (derzeit Hover) der Inhalt sichtbar oder unsichtbar wird. Natürlich kann dieser Text sehr sehr lange werden und muss entsprechend in der kleinen Ansicht mit Ellipsis dargestellt werden.",
+                finished: false,
+                importance: 2,
+                due: moment().add(1, 'days')
+            },
+            {
+                id: 2,
+                title: "Einkaufen",
+                content: "- Butter\n- Bier\n- Eier\n- Brot",
+                finished: false,
+                importance: 4,
+                due: moment().add(3, 'days')
+            }
+        ];
+        var notesTemplate = $('#notes-template').html();
+        var compiledNotesTemplate = Handlebars.compile(notesTemplate);
+        var renderData = function () {
+            var generatedHtml = compiledNotesTemplate(notes);
+            console.log(generatedHtml);
+            $('#notes-table').html(generatedHtml);
+        };
+        renderData();
+    });
+})(jQuery);
