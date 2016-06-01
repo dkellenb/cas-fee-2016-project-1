e65cc320d8c4b0cde31fa784df5901df92699fd1
bruede <benj.ruede@gmail.com>
2016-06-01 13:06:58 +0200

    simple version of toggleing edit mode

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
     var DateFormats = {
         short: "DD.MM.YYYY",
         long: "dddd DD.MM.YYYY HH:mm"
+        input: "YYYY-MM-DD"
     };
     Handlebars.registerHelper("formatDate", function(datetime, format) {
         if (moment) {
@@ -43,248 +44,91 @@
         }
     });
 
-    Handlebars.getTemplate = function(name) {
-        if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
-            $.ajax({
-                url : 'hbs/' + name + '.hbs',
-                success : function(data) {
-                    if (Handlebars.templates === undefined) {
-                        Handlebars.templates = {};
-                    }
-                    Handlebars.templates[name] = Handlebars.compile(data);
-                },
-                async : false
-            });
-        }
-        return Handlebars.templates[name];
-    };
-
 })(Handlebars);
-
-/**
- * JQuery extension.
- */
-(function ($) {
-    $.urlParam = function(name){
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        if (results==null){
-            return null;
-        }
-        else{
-            return results[1] || 0;
-        }
-    }
-})($);
 
 /**
  * Notes application.
  */
 (function ($) {
     $(function () {
+        var notes = [
+            {
+                title: "CAS-FEE Projekt bearbeiten",
+                content: "Hier ist ein Text welcher einen möglichen Inhalt einer Notiz darstellt. Dieser kann relative sehr gross sein. Die Idee ist dass durch einen Klick (derzeit Hover) der Inhalt sichtbar oder unsichtbar wird. Natürlich kann dieser Text sehr sehr lange werden und muss entsprechend in der kleinen Ansicht mit Ellipsis dargestellt werden.",
+                finished: false,
+                importance: 2,
+                due: moment().add(1, 'days'),
+                isEditable: false
+            },
+            {
+                title: "Einkaufen",
+                content: "- Butter\n- Bier\n- Eier\n- Brot",
+                finished: true,
+                importance: 4,
+                due: moment().add(3, 'days'),
+                isEditable: false
+            }
+        ];
 
         /**
-         * Notes repository which is capable of selecting, inserting, updating and deleting notes.
+         * Render the Data from the notes Array with Handelbarstemplates.
          */
-        var notesRepository = function () {
-
-            /**
-             * Gets all persisted notes.
-             *
-             * @returns {Array} the persisted notes
-             */
-            var getNotes = function () {
-                var serializedNotes = localStorage.getItem('notes');
-                console.log("Load notes: " + serializedNotes);
-                var notes = serializedNotes === undefined || serializedNotes === null ? [] : JSON.parse(serializedNotes);
-                return notes;
-            };
-
-            /**
-             * Gets a note with the given id.
-             *
-             * @param id the id of the note
-             * @returns {*|{}} null or the object
-             */
-            var getNote = function (id) {
-                var _id = parseInt(id);
-                return getNotes().find(function (note) { return note.id === _id; });
-            };
-
-            /**
-             * Save all notes.
-             *
-             * @param notes saves all notes.
-             * @returns {Array} the persisted notes
-             */
-            var saveNotes = function (notes) {
-                var serializedNotes = notes === undefined || notes === null ? "[]" : JSON.stringify(notes);
-                localStorage.setItem('notes', serializedNotes);
-                return notes;
-            };
-
-            /**
-             * Saves a given note. Updates an existing persisted one.
-             *
-             * @param note the note to persist
-             * @returns the persisted note
-             */
-            var saveNote = function (note) {
-                var notes = getNotes();
-                if (note.id === undefined || note.id === null) {
-                    note.id = notes.length === 0 ? 1 : Math.max.apply(Math, notes.map(function (n) { return n.id })) + 1;
-                    notes.push(note);
-                } else {
-                    var index = notes.findIndex(function (noteEntry) {
-                        return noteEntry.id === note.id
-                    });
-                    if (index > -1) {
-                        notes[index] = note;
-                    } else {
-                        notes.push(note);
-                    }
-                }
-                saveNotes(notes);
-                return note;
-            };
-
-            /**
-             * Deletes a given note.
-             *
-             * @param id if the note to be deleted
-             * @return {Array} the updated notes
-             */
-            var deleteNote = function (id) {
-                var notes = getNotes();
-                var index = notes.findIndex(function (noteEntry) { return noteEntry.id === note.id });
-                if (index > -1) {
-                    notes.splice(index, 1);
-                    saveNotes(notes);
-                }
-                return notes;
-            };
-
-            // Return public interface.
-            return {
-                getNotes: getNotes,
-                getNote: getNote,
-                saveNotes: saveNotes,
-                saveNote: saveNote,
-                deleteNote: deleteNote
-            };
-        }();
+        var renderData = function () {
+            var generatedHtml = Handlebars.getTemplate('notes-template')(notes);
+            $('#notes-table').html(generatedHtml);
+            registerEvents();
+        };
 
         /**
-         * The edit form controller. Dependes on:
-         * - notesRepository
+         * Function for register all Events.
          */
-        var formController = function ($, notesRepository) {
+        var registerEvents = function () {
+            $('.edit-button').on("click", function (event) {
+                toggleNoteEditable(event.target.getAttribute("data-note-index"));
+            });
 
-            /**
-             * Sets all values of the note to the form.
-             * 
-             * @param note
-             */
-            var setNote = function (note) {
-                $('title').text("Edit note " + note.id);
-                $('.title').text("Edit note " + note.id);
-                $('#title').val(note.title);
-                $('#description').val(note.content);
-                // TODO importance
-                $('#dateTo').val(note.due);
-            };
+            $('.save-button').on("click", function (event) {
+                saveNote(event.target.getAttribute("data-note-index"));
+            });
+        };
 
-            /**
-             * Retrieve note values from the form.
-             *
-             * @return note
-             */
-            var getNote = function () {
-                // TODO add some input validation
-                return {
-                    id: $.urlParam('id'),
-                    title: $('#title').val(),
-                    content: $('#description').val(),
-                    finished: false,
-                    importance: parseInt($("input:radio[name=importance]:checked").val()),
-                    due: moment($("#dateTo").val())
-                };
-            };
+        /**
+         * Toggle isEditable on note at position of the noteIndex in Array notes.
+         * @param noteIndex
+         */
+        var toggleNoteEditable = function (noteIndex) {
+            notes[noteIndex].isEditable = !notes[noteIndex].isEditable;
+            console.log('Note ' + noteIndex + ' isEditable: ' + notes[noteIndex].isEditable);
+            renderData();
+        };
 
-            /**
-             * Persists the form.
-             */
-            var save = function () {
-                var note = getNote();
-                console.log(note);
-                notesRepository.saveNote(note);
-                window.location.href = "./";
-            };
-
-            /**
-             * Initializes the form.
-             */
-            var initialize = function () {
-                $('#save').on("click", function(event) {
-                    save();
-                });
-                
-                var id = $.urlParam('id');
-                if (id === null || id === undefined) {
-                    return;
-                }
-                
-                var note = notesRepository.getNote(id);
-                if (note === null || note === undefined) {
-                    return;
-                }
-
-                setNote(note);
-            };
-
-            // Return public interface.
-            return {
-                initialize: initialize
-            }
-
-        }($, notesRepository);
-
-        var overviewController = function ($, notesRepository) {
-
-            var renderData = function (notes) {
-                var generatedHtml = Handlebars.getTemplate('notes-template')(notes);
-                $('#notes-table').html(generatedHtml);
-            };
-
-            var registerEvents = function () {
-                $('.edit-button').on("click",function(event) {
-                    toggleNodeEditable(event.target.getAttribute("data-note-id"));
-                });
-            };
-
-            var toggleNodeEditable = function (id) {
-                window.location.href = "./edit.html?id=" + id;
-            };
-
-            var initialize = function () {
-                var notes = notesRepository.getNotes();
-                renderData(notes);
-                registerEvents();
-            };
-
-            // Return public interface.
-            return {
-                initialize: initialize
-            }
-
-        }($, notesRepository);
-
-        if (window.location.href.indexOf('edit.html') > -1) {
-            formController.initialize();
-        } else {
-            overviewController.initialize();
-        }
-
+        /**
+         * save note data to the node at the noteIndex.
+         * @param noteIndex
+         */
+        var saveNote = function (noteIndex) {
+            var formData = new FormData(document.querySelector('#edit-form-note-' + noteIndex));
+            console.log(formData);
+            toggleNoteEditable(noteIndex);
+        };
+        renderData();
     });
+
+    Handlebars.getTemplate = function (name) {
+        if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+            $.ajax({
+                url: 'hbs/' + name + '.hbs',
+                success: function (data) {
+                    if (Handlebars.templates === undefined) {
+                        Handlebars.templates = {};
+                    }
+                    Handlebars.templates[name] = Handlebars.compile(data);
+                },
+                async: false
+            });
+        }
+        return Handlebars.templates[name];
+    };
 
 })(jQuery);
 
