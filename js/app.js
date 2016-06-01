9406dd1b8e65f7edf4f3f56315b5f1deb1151c27
bruede <benj.ruede@gmail.com>
2016-06-01 13:35:01 +0200

    moved handelbars template loading function in handelbar scope

--- js/app.js
+++ js/app.js
@@ -44,6 +44,22 @@
         }
     });
 
+    Handlebars.getTemplate = function(name) {
+        if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+            $.ajax({
+                url : 'hbs/' + name + '.hbs',
+                success : function(data) {
+                    if (Handlebars.templates === undefined) {
+                        Handlebars.templates = {};
+                    }
+                    Handlebars.templates[name] = Handlebars.compile(data);
+                },
+                async : false
+            });
+        }
+        return Handlebars.templates[name];
+    };
+
 })(Handlebars);
 
 /**
@@ -113,22 +129,5 @@
         };
         renderData();
     });
-
-    Handlebars.getTemplate = function (name) {
-        if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
-            $.ajax({
-                url: 'hbs/' + name + '.hbs',
-                success: function (data) {
-                    if (Handlebars.templates === undefined) {
-                        Handlebars.templates = {};
-                    }
-                    Handlebars.templates[name] = Handlebars.compile(data);
-                },
-                async: false
-            });
-        }
-        return Handlebars.templates[name];
-    };
-
 })(jQuery);
 
