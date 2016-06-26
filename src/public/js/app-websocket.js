4ce2160adf9dd092aa671d61a65052d430db824c
bruede <benj.ruede@gmail.com>
2016-06-26 20:39:25 +0200

    -fixed bug with wrong propertie name

--- src/public/js/app-websocket.js
+++ src/public/js/app-websocket.js
@@ -7,19 +7,19 @@
     var socket = io('/');
     socket.on('created', function (idObject) {
         console.log('created: ' + idObject.id);
-        if (idObject.sennderId !== namespace.socket.id) {
+        if (idObject.senderId !== namespace.socket.id) {
             namespace.notesController.onExternalNoteCreation(idObject.id);
         }
     });
     socket.on('deleted', function (idObject) {
         console.log('deleted: ' + idObject.id);
-        if (idObject.sennderId !== namespace.socket.id) {
+        if (idObject.senderId !== namespace.socket.id) {
             namespace.notesController.onExternalNoteDelete(idObject.id);
         }
     });
     socket.on('updated', function (idObject) {
         console.log('updated: ' + idObject.id);
-        if (idObject.sennderId !== namespace.socket.id) {
+        if (idObject.senderId !== namespace.socket.id) {
             namespace.notesController.onExternalNoteUpdate(idObject.id);
         }
     });
